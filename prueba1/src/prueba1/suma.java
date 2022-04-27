66c3caf1a2f530ede3bc63adb913bd43712819ee
rositamartineez <rosarositamg2000@gmail.com>
2022-04-27 09:56:03 +0200

    primer commit

---
+++ prueba1/src/prueba1/suma.java
@@ -0,0 +1,22 @@
+package prueba1;
+
+import java.util.Scanner;
+
+public class suma {
+
+	public static void main(String[] args) {
+		
+		int num1, num2;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Introduce el primer número: ");
+		num1 = sc.nextInt();
+		
+		System.out.println("Introduce el segundo número: ");
+		num2 = sc.nextInt();
+		
+		System.out.println(num1 + " + " + num2 + " = " + (num1+num2) );
+
+	}
+
+}
